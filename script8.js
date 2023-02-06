b7ca21e61689e88ca3d19a3f58c12b824e1ff9a7
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-02-06 18:53:14 +0300

    task_8

---
+++ script8.js
@@ -0,0 +1,18 @@
+let a = 10, b = 2;
+alert(a + b);
+alert(a - b);
+alert(a * b);
+alert(a / b);
+
+let a1 = 10, b1 = 5;
+sum = a1 + b1;
+alert(sum);
+
+let a2 = 1, b2 = 2, c2 = 3;
+alert(a2 + b2 + c2);
+
+let a3 = 10, b3 = 5;
+let d3 = 7;
+c3 = a3 - b3;
+result = c3 + d3;
+alert(result);
