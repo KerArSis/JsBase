9534c8beb67a6359dfbb552cb0bf9753311bb0ed
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-02-06 18:45:24 +0300

    task_6

---
+++ script6.js
@@ -0,0 +1,21 @@
+let num = 888;
+alert(num);
+
+let a = 1;
+let b = 2;
+let c = 3;
+
+let d = 2, f = 4, e = 6;
+
+let x, y, z;
+aa = 2;
+bb = 4;
+cc = 6;
+
+let one;  
+
+one = 10;    
+alert(hunt); 
+
+one = 20;     
+alert(one); 
