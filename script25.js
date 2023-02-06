d1c527170e783585849b69ffa7adde1249d4294c
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-02-06 20:34:34 +0300

    task_26

--- script25.js
+++
@@ -1,7 +0,0 @@
-alert(true + 3);
-alert(true + true);
-alert(true - true);
-alert(true + false);
-alert('1' + true);
-alert(String(true) + 1);
-alert(String(true) + Number(true));
---
+++ script26.js
@@ -0,0 +1,20 @@
+let a = Boolean(3);
+alert(a);
+
+let b = Boolean(0);
+alert(b);
+
+let c = Boolean(-1);
+alert(c);
+
+let d = Boolean(-0);
+alert(d);
+
+let e = Boolean('abc');
+alert(e);
+
+let f = Boolean('');
+alert(f);
+
+let g = Boolean(3 * 'abs');
+alert(g);
