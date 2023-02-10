4a195cf6a2d79131b7d30e74c0b172dfeca52226
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-02-10 12:30:55 +0300

    task_35

--- script34.js
+++ script34.js
@@ -1,12 +1,36 @@
-document.write('text');
+let num1 = 1;
+let num2 = 2;
+alert('сумма: ', num1 + num2); 
 
-document.write('<p>Alex</p>');
+let a = 1;
+let b = 2;
+alert(a + b);
 
-let str = 'text';
-document.write('<p>Jason</p>');
+let num = '123';
+let sum = Number(num[0]) + Number(num[1]) + Number(num[2]);
+alert(sum);
 
-document.write('1<br>');
-document.write('2<br>');
-document.write('3<br>');
-document.write('4<br>');
-document.write('5<br>');
+let num3 = '123';
+alert(num3[0]);
+
+let a1 = 0;
+alert(++a1);
+
+let num4 = '123';
+alert(num4.length);
+
+let a2 = 24 / 60 / 60;
+alert(a2.toFixed(4));
+
+let num5 = 123;
+let str = String(num5);
+alert(str.length);
+
+let num6 = 123;
+let str1 = String(num6);
+alert(str1[str1.length-1]);
+
+let a3 = '123';
+let b3 = '456';
+let s = Number(a3) + String(b3);
+alert(Number(s));
