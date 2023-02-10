42211dd25b33bddd71fa10a0d479175ab5b2317d
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-02-10 12:26:37 +0300

    task_34

---
+++ script34.js
@@ -0,0 +1,12 @@
+document.write('text');
+
+document.write('<p>Alex</p>');
+
+let str = 'text';
+document.write('<p>Jason</p>');
+
+document.write('1<br>');
+document.write('2<br>');
+document.write('3<br>');
+document.write('4<br>');
+document.write('5<br>');
