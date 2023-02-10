3e3112f59cb10b9663d38f975e783c0f5c8a5e8a
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-02-10 12:07:44 +0300

    task_29

--- script28.js
+++
@@ -1,8 +0,0 @@
-let a = 'abcde';
-alert(a[a.length - 1]);
-
-let b = 'abcde';
-alert(b[b.length - 2]);
-
-let c = 'abcde';
-alert(c[c.length - 3]);
---
+++ script29.js
@@ -0,0 +1,11 @@
+let x = '12345';
+alert(Number(x[0]) + Number(x[1]) + Number(x[2]) + Number(x[3]) + Number(x[4]));
+
+let y = String(12345);
+alert(Number(y[0]) + Number(y[1]) + Number(y[2]) + Number(y[3]) + Number(y[4]));
+
+let z = String(12345);
+alert(Number(z[0]) * Number(z[1]) * Number(z[2]) * Number(z[3]) * Number(z[4]));
+
+let g = String(12345);
+alert((g[4]) + (g[3]) + (g[2]) + (g[1]) + (g[0]));
