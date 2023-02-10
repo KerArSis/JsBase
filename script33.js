206e1c2e1b02cbe9aaf0b8b9ff366270ae8e7e20
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-02-10 12:21:25 +0300

    task_33

---
+++ script33.js
@@ -0,0 +1,12 @@
+alert('Сколько вам лет:' + prompt('Сколько вам лет?')); 
+
+let num1 = prompt('Введите первое число');
+let num2 = prompt('Введите второе число');
+alert(Number(num1) + Number(num2)); 
+
+let sguare = prompt('Сторона квадрата = ');
+alert(sguare*sguare);
+
+let num3 = prompt('Введите первое число');
+let num4 = prompt('Введите второе число');
+alert(Number(num3) + Number(num4)); 
