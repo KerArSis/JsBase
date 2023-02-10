3c20c35120793804bab1c1cf951c0124f0727651
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-02-10 12:54:07 +0300

    task_41

--- script40.js
+++
@@ -1,5 +0,0 @@
-let arr = [1, 2, 3, 4, 5, 6];
-alert(arr.length);
-
-let arr1 = [1, 2, 3, 4, 5, 6];
-alert(arr1[arr1.length - 1])
---
+++ script41.js
@@ -0,0 +1,17 @@
+let arr = ['a', 'b', 'c'];
+arr[0] = 1;
+arr[1] = 2;
+arr[2] = 3;
+
+let arr1 = [1, 2, 3, 4];
+arr1[0] += 3;
+arr1[1] += 3;
+arr1[2] += 3;
+arr1[3] += 3;
+alert(arr1)
+
+let arr2 = [1, 2, 3];
+arr2[0]++;
+arr2[1]++;
+arr2[2]++;
+alert(arr2);
