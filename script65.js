ba4faafd266423093ed05b722b08110f0998eb72
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-02-11 17:35:22 +0300

    task_65

---
+++ script65.js
@@ -0,0 +1,7 @@
+console.log( typeof {x: 1, y: 2, z: 3} );
+let arr1 = [1, 2, 3];
+console.log( typeof arr1 );
+let arr2 = [1, 2, 3];
+console.log( typeof arr2[0] );
+let arr3 = ['1', '2', '3'];
+console.log( typeof arr3[0] );
