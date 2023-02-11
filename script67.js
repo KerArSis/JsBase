46cd8c54bc183f79da81c75ef487c7914b848114
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-02-11 17:40:06 +0300

    task_67

---
+++ script67.js
@@ -0,0 +1,20 @@
+let test1 = {x: 1, y: 2, z: 3};
+console.log(test1);
+
+let test2 = {x: 1, y: 2, z: 3};
+console.log(test2.x);
+
+let test3 = [1, 2, 3];
+console.log(test3);
+
+let test4 = [1, 2, 3];
+console.log(test4[1]);
+
+let test5 = [1, 2, 3];
+let test6 = 1;
+console.log(test5);
+
+let test7 = [1, 2, 3];
+let test8 = 1;
+console.log(test7[test8]);
+
