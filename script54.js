3d2619199e87cdfd48adc110c89e588d20b51cc9
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-02-11 17:12:31 +0300

    task_55

--- script54.js
+++
@@ -1,5 +0,0 @@
-let obj = {};
-obj['a'] = 1;
-obj['b'] = 2;
-obj['c'] = 3;
-console.log(obj);
---
+++ script55.js
@@ -0,0 +1,4 @@
+let obj = {25: 'a', 88: 'b', 15: 'c'};
+console.log(obj[88]); 
+console.log(obj[15]); 
+console.log(obj[25]);
