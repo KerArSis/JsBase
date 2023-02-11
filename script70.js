082e3c2e31a16fab8f4cfcb0b7316ff8d5b5d033
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-02-11 21:49:18 +0300

    task_71

--- script70.js
+++
@@ -1,3 +0,0 @@
-let arr = [1, 2, 3, 4, 5];
-let res = arr[1] + arr[2];
-console.log(res);
---
+++ script71.js
@@ -0,0 +1,13 @@
+let obj1 = {x: 1, y: 2, z: 3};
+console.log(obj1[x]);
+
+let obj2 = {x: 1, y: 2, z: 3};
+let key = 'x';
+console.log(obj2.key);
+
+let obj3 = {x: 1, y: 2, z: 3};
+let sum = obj3[x] + obj3[y] + obj3[x];
+console.log(obj3);
+
+let obj4 = {x: 1, y: 2, z: 3};
+console.log(obj4.length);
