3fcc6cde3f7f70760f4c5c3da0fe14e83003f918
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-02-17 16:16:49 +0300

    task_80

--- script80.js
+++ script80.js
@@ -1,22 +1,22 @@
 let test1 = '3';
 let test2 = '3';	
-if (test1 == test2) {
+if (test1 != test2) {
 	console.log('+++');
 } else {
 	console.log('---');
 }
 
-let test3 = 3;
+let test3 = '3';
 let test4 = '3';
-if (test3 === test4) {
+if (test3 !== test4) {
 	console.log('+++');
 } else {
 	console.log('---');
 }
 
 let test5 = 3;
-let test6 = 3;	
-if (test5 === test6) {
+let test6 = '3';	
+if (test5 !== test6) {
 	console.log('+++');
 } else {
 	console.log('---');
