803ddad3b66ecfbe045024920568df5a64e272ce
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-02-17 16:06:19 +0300

    task_76

--- script75.js
+++
@@ -1,6 +0,0 @@
-let test = 10;
-if (test != 10) {
-	console.log('+++'); 
-} else {
-	console.log('---');
-}
---
+++ script76.js
@@ -0,0 +1,15 @@
+let test1 = 1;
+let test2 = 2;
+if (test2 > test1) {
+	console.log('+++');
+} else {
+	console.log('---'); // сработает это
+}
+
+let test3 = 1;
+let test4 = 2;
+if (test3 == test4) {
+	console.log('+++');
+} else {
+	console.log('---'); // сработает это
+}
