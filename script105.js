dd0a7a309cdf48dfde6a5e306e3cc2df971445b9
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-02-26 15:32:07 +0300

    task_105

---
+++ script105.js
@@ -0,0 +1,23 @@
+let string = 'abc';
+
+   if (string[0] == 'a' || string[0] == 'A') {
+      console.log('yes');
+   } else {
+      console.log('no');
+   }
+
+let string1 = 'xbox';
+
+if (string1[string1.length - 1] == 'x' || string1[string1.length - 1] == 'X') {
+      console.log('yes');
+   } else {
+      console.log('no');  
+   }
+
+let string2 = 'abc!';
+
+   if (string2[0] == 'a' || string2[0] == 'b') {
+      console.log('yes');
+   } else {
+      console.log('no');  
+   }
