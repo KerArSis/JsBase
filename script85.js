73aa9ab6b2241c402d1a1f07eeafa1c986253a77
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-02-26 14:36:56 +0300

    task_86

--- script85.js
+++
@@ -1,5 +0,0 @@
-if ( !(num1 >= 0 || num2 <= 10)) {
-	console.log('+++');
-} else {
-	console.log('---');
-}
---
+++ script86.js
@@ -0,0 +1,13 @@
+let test = true;
+if (test === true) {
+    console.log('+++');
+} else {
+    console.log('---');
+}
+
+let test1 = false;
+if (test1 === false) {
+    console.log('+++');
+} else {
+    console.log('---');
+}
