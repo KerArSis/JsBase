c3eb79cbac5647b2ebdccb3e72d9e3b60b78136e
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-02-26 15:35:38 +0300

    task_107

---
+++ script107.js
@@ -0,0 +1,15 @@
+let a = 10;
+
+    if (a % 2 == 0) {
+        console.log('делится нацело');
+    } else {
+        console.log('делится с остатком')
+    }
+
+let b = 9;
+
+   if (b % 3 == 0) {
+      console.log('делится нацело')
+   } else {
+      console.log('делится с остатком')
+   }
