c7e740e0e945ff107507a262daf9cd6d104b5323
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-02-26 15:26:37 +0300

    task_103

---
+++ script103.js
@@ -0,0 +1,13 @@
+let min = 60;
+
+   if (min >= 0 && min <= 20) {
+      console.log('1 четверть');
+   }
+
+   if (min >= 21 && min <= 41) {
+      console.log('2 четверть');
+   }
+
+   if (min >= 42 && min <= 60) {
+      console.log('3 четверть');
+   }
