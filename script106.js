77dfb4216a6c608498e057b4513710ec2a2531cb
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-02-26 15:33:51 +0300

    task_106

---
+++ script106.js
@@ -0,0 +1,14 @@
+let num = 10;
+let str = String(num);
+if (str[str.length - 1] == 0 || str[str.length - 1] == 1) {
+    console.log('+++');
+} else {
+    console.log('---');
+}
+
+let num1 = 2;
+if (num1 % 2 == 0) {
+    console.log('yes')
+ } else {
+    console.log('no')
+ }
