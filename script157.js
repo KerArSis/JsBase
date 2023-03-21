7047446e510fa3488a11ca17fbe5fe808859c837
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-03-21 21:31:21 +0300

    task_157

---
+++ script157.js
@@ -0,0 +1,19 @@
+// 1 
+
+function cube(a) {
+   console.log(Math.pow(a, 3));
+}
+
+cube(2);
+
+// 2
+
+function negative(b) {
+   if (b >= 0) {
+      console.log('+++')
+   } else {
+      console.log('---')
+   }
+}
+
+negative(-2);
