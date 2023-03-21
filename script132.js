79db89f85c5378b16ec9cf4a4e446367f7800a53
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-03-21 20:36:38 +0300

    task_132

---
+++ script132.js
@@ -0,0 +1,20 @@
+let str = 'Wery interesting!!';
+   let arr = str.split('') 
+   let count = {};         
+   let result = 0 
+
+   for (let item of arr) {
+      if (count[item] === undefined) {
+         count[item] = 1;
+      } else {
+         count[item]++;
+      }
+   }
+
+   for (let item in count) {
+      if(count[item] > 1) {
+         result += count[item]
+      };
+   };
+
+   console.log(result);
