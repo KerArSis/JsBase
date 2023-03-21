b51bac1d9eac30f236591ddffaf502c50d98598f
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-03-21 19:21:30 +0300

    task_117

---
+++ script117.js
@@ -0,0 +1,20 @@
+let res = 0;
+for (let i = 2; i <= 100; i += 2) {
+    res += i;
+} 
+
+console.log(res);
+
+let res1 = 0;
+for (let i = 1; i <= 99; i += 2) {
+    res1 += i;
+}
+
+console.log(res1); 
+
+let res2 = 1;
+for (let i = 1; i <= 20; i += 1) {
+    res2 *= i; 
+}
+
+console.log(res2);
