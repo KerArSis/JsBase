7fa62355803172d864d06cfa74e0f56e019156bd
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-03-21 19:46:54 +0300

    task_130

---
+++ script130.js
@@ -0,0 +1,36 @@
+let arr = ['a', 'b', 'c', 'd', 'e'];
+let flag = false;
+
+for (let elem of arr) {
+    if (elem == 'c') {
+        flag = true;
+        break;
+    }
+} 
+
+if (flag === true) {
+    console.log('+++');
+} else {
+    console.log('---');
+}
+
+
+let num = 4;
+   let flag2 = true;
+   let i = 2;
+
+   while (i < num) {
+      if (num % i == 0) {
+         flag2 = !flag2;
+         break;
+
+      } else {
+         i += 1;
+      };
+   };
+
+   if (flag2) {
+      console.log(`Число ${num} является простым`)
+   } else {
+      console.log(`Число ${num} не является простым`)
+   }
