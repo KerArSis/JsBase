1088b93b991b98fa68b3253f6ede60f419ef97e6
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-03-21 20:43:52 +0300

    task_135

---
+++ script135.js
@@ -0,0 +1,12 @@
+let obj = { a: 10, b: 20, c: 30, d: 40, e: 50 };
+      let sum = 0;
+
+      for (let elem in obj) {
+         if (obj[elem] == 10 || obj[elem] == 20) {
+            sum += obj[elem];
+         } else {
+            continue;
+         };
+      };
+
+      console.log(sum);
