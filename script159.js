da5c59e4b9b1f7acdaf2bd58e41d053ba41dcf78
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-03-21 21:35:51 +0300

    task_160

--- script159.js
+++
@@ -1,9 +0,0 @@
-let param1 = 1;
-      let param2 = 5;
-      let param3 = 3;
-
-      function three(a, b, c) {
-         return console.log(a + b + c);
-      };
-
-      three(param1, param2, param3);
