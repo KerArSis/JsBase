232515b6a401b11b374ca9e583dbb12de8050a8a
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-03-21 20:42:14 +0300

    task_134

---
+++ script134.js
@@ -0,0 +1,9 @@
+let arr = [10, 20, 30, 40, 21, 32, 51];
+
+for (let i of arr) {
+    if (String(i)[0] == '1' || String(i)[0] == '2') {
+            console.log(i);
+    } else {
+            continue;
+    };
+};
