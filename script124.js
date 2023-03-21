8edbe7b37e5841a2e02ddb6e24e53bf50c05669c
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-03-21 19:37:41 +0300

    task_125

--- script124.js
+++
@@ -1,6 +0,0 @@
-let num;
-
-for (let i = 1; i <= 9; i++) {
-    num = 3;
-}
-console.log(num);    
---
+++ script125.js
@@ -0,0 +1,7 @@
+for (let i = 0; i <= 9; i++) {
+    let num = 3;
+
+    for (let j = 0; j <= 9; j++) {
+        console.log(num);
+    }
+}   
