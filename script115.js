caf075e245e2be054804749ad809ec2f00a2e810
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-03-21 19:17:45 +0300

    task_115

---
+++ script115.js
@@ -0,0 +1,14 @@
+let arr = ['a', 'b', 'c', 'd', 'e'];
+for (let i = 0; i <= arr.lenght - 1; i++) {
+console.log(arr[i]);
+}
+
+let arr1 = ['a', 'b', 'c', 'd', 'e'];
+for (let i = 1; i <= arr1.length - 2; i++) {
+console.log(arr[i]);
+}
+
+let arr2 = ['a', 'b', 'c', 'd', 'e'];
+for (let i = arr2.length - 1; i > 0; i--) {
+console.log(arr2[i]);
+}
