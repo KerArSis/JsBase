b224250c029bfeefbd52ceddba5a7354626c7c6d
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-03-21 21:40:33 +0300

    task_161

---
+++ script161.js
@@ -0,0 +1,15 @@
+// 1
+function func(num) {
+   return num ** 3;
+}
+
+let res = func(3);
+console.log(res);
+
+// 2
+function func(num) {
+   return Math.sqrt(num);
+}
+
+let res2 = func(3) + func(4); 
+console.log(res2);
