e6975fa6e6ceac99cc721457d0222fa2e3b07d21
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-03-21 21:51:15 +0300

    task_168

---
+++ script168.js
@@ -0,0 +1,27 @@
+// 1
+let x = 5, y = 5
+function func(a, b) {
+    return a == b;    
+}
+console.log(func(x, y));
+
+// 2
+let x2 = 5, y2 = 4
+function func(a, b) {
+    return a != b;    
+}
+console.log(func(x2, y2)); 
+
+// 3
+let x3 = 4, y3 = 7
+function func(a, b) {
+    return a + b >= 10;    
+}
+console.log(func(x3, y3)); 
+
+// 4
+let x4 = 1
+function func(num) {
+    return num >= 0;    
+}
+console.log(func(x4));
