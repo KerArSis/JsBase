ba3e1c5b2c0cd4143714ddb9339d6b4b7c34ff46
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-03-29 13:02:34 +0300

    task_210

--- script209.js
+++
@@ -1,7 +0,0 @@
-function func() {
-	return ['John', 'Smit', 'development', 
-		'programmer', 2000]; 
-}
-let [name, surname, department, position, salary] = func();
-console.log(surname);
-console.log(name);
