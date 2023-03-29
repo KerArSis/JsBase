c779b5ba3da224c4f9e8d9ca719b3017a7da7df2
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-03-29 13:32:46 +0300

    task_215

--- script214.js
+++
@@ -1,8 +0,0 @@
-function func() {
-    return (
-        String(new Date().getFullYear()) + " " +
-        String(new Date().getMonth() + 1) + " " +
-        String(new Date().getDate())
-    );
-}
-console.log(func());
