c46e7ca15d00f39f9d5fab93bda0a3ff2c8b4510
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-03-29 13:31:02 +0300

    task_214

--- script213.js
+++
@@ -1,9 +0,0 @@
-let arr = ["John", "Smit", "development"];
-
-let [name, surname, department, programmer = 'programmer', year = 200] = arr
-
-console.log(name)
-console.log(surname)
-console.log(department)
-console.log(programmer)
-console.log(year)
