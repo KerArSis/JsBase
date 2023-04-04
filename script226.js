3cb47a171eb19d3948724bceb0d89a5f46edf1d8
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-04-04 17:59:55 +0300

    task_227

--- script226.js
+++
@@ -1,9 +0,0 @@
-let month = new Date();
-let m = month.getMonth();
-let months = [
-	'янв', 'фев', 'мар', 'апр', 'май', 
-		'июн', 
-	'июл', 'авг', 'сен', 'окт', 'ноя', 
-		'дек' 
-];
-console.log(months[m])
