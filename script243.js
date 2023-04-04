e97653e479585c1e7dcbd270374b7fd21ef4c58c
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-04-04 18:45:10 +0300

    task_243

---
+++ script243.js
@@ -0,0 +1,12 @@
+let button1 = document.querySelector('#button');
+button.addEventListener('click', func1);
+let button2 = document.querySelector('#button');
+button.addEventListener('click', func2);
+
+function func1() {
+	console.log(1);
+}
+
+function func2() {
+	console.log(2);
+}
