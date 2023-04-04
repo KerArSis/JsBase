b54e3be5ed1cdba0901156eeb5ef418d7026a9da
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-04-04 18:27:45 +0300

    task_236

--- script235.js
+++
@@ -1,8 +0,0 @@
-let now = new Date();
-let year = now.getFullYear();
-for (let month = 0; month <= 11; month++) {
-let date = new Date(year, month, 1);
-if (date.getDay() == 0) {
-    console.log(year + "-" + month + "-1");
-}
-}
