918e1b4058b1fea336af82fe2a60854c24af0bfe
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-04-04 19:14:18 +0300

    task_249

---
+++ script249.js
@@ -0,0 +1,20 @@
+let button = document.querySelector('#elem');
+
+let button1 = document.querySelector('#switching');
+
+console.log(elem.id);   
+console.log(elem.type);
+
+
+function switcher(){
+	if (elem.type == 'text'){
+		elem.type = 'button';
+	}
+	else{
+		elem.type = 'text'
+		elem.textContent = 'Hello'
+		
+	}
+}
+
+button1.addEventListener('click', switcher)
