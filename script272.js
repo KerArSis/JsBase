98d6990e6e22fd8184509ccff3562f95d563097a
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-04-10 21:22:53 +0300

    task_272

---
+++ script272.js
@@ -0,0 +1,6 @@
+let button = document.querySelector('button');
+let elem   = document.querySelector('p');
+
+button.addEventListener('click', function() {
+	elem.innerHTML = '<b>Yunus Mustafaev</b>';
+});
