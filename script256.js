e367c1113a69b6242db2d5b5dfec8e1fc027d199
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-04-10 20:49:33 +0300

    task_256

---
+++ script256.js
@@ -0,0 +1,8 @@
+let elems = document.querySelectorAll('.www');
+let but = document.querySelector('#but')
+let i = 0;
+but.addEventListener('click', function func(){
+	for (let elem of elems) {
+		elem.textContent = elem.textContent + elem.textContent;
+	}
+})
