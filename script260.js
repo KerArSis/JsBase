92abb4e300e6b079554a45afd0c013fb1fbc3558
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-04-10 20:58:24 +0300

    task_260

---
+++ script260.js
@@ -0,0 +1,11 @@
+let elems = document.querySelectorAll('p');
+
+for (let elem of elems) {
+	elem.addEventListener('click', func);
+}
+
+function func() {
+	console.log(this.textContent);
+	this.removeEventListener('click', func); 
+	this.textContent += "!"
+}
