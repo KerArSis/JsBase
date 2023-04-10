b21fca6cf35c4a2f0d0934b430dfac1e59697a86
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-04-10 21:12:32 +0300

    task_267

--- script266.js
+++
@@ -1,3 +0,0 @@
-let elem = document.querySelector('#elem');
-let parent = elem.closest('.www');
-console.log(parent.id);
---
+++ script267.js
@@ -0,0 +1,5 @@
+let elems = document.getElementsByClassName('www');
+
+for (let elem of elems) {
+	elem.innerHTML = '!!!';
+}
