40c05d85c66b891785200f1af217d285a6b43184
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-04-10 21:27:11 +0300

    task_274

---
+++ script274.js
@@ -0,0 +1,7 @@
+let elem = document.querySelector('#elem');
+let text = document.querySelector('p')
+
+elem.addEventListener('blur', function func(){
+	text.textContent += ' ' + elem.value;
+	elem.value = ' ';
+})    
