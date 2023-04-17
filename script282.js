d57295540bf1f40bb0c6a27b1637e0f687b5ba63
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-04-17 20:45:22 +0300

    task_283

--- script282.js
+++
@@ -1,7 +0,0 @@
-let select = document.querySelector('#select');
-let but = document.getElementsByTagName('#button');
-let inp = document.getElementsByTagName('#input');
-
-but.addEventListener('change', function(){
-	console.log(this.value);
-});
---
+++ script283.js
@@ -0,0 +1,6 @@
+let select = document.querySelector('#select');
+let button = document.querySelector('#button');
+
+button.addEventListener('click', function() {
+	select.value = 'one';
+});
