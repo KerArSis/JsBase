c995ba117c59da729b0a7dc9cb915adeb2858208
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-04-17 21:09:21 +0300

    task_295

--- script294.js
+++
@@ -1,7 +0,0 @@
-let elem = document.querySelector('#elem');
-
-elem.addEventListener('click', function(event) {
-	event.preventDefault('/');
-	this.elem = this.elem + ' (' + this.href + ')';
-	console.log('Вы не можете перейти по этой ссылке!');
-});
