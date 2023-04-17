be66f7ffe319a82c31a7b7f75451ce95013008b2
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-04-17 20:57:19 +0300

    task_289

--- script288.js
+++
@@ -1,8 +0,0 @@
-let elem = document.querySelector('#elem');
-
-elem.addEventListener('click', function(event) {
-	console.log(event);
-	if (event.ctrlKey == true){
-		console.log('Ctrl Clicked')
-	}
-});
