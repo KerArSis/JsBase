dc2729383a456f3356cee9b8724b623e31f30ea5
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-04-17 20:58:59 +0300

    task_290

--- script289.js
+++
@@ -1,5 +0,0 @@
-let elem = document.getElementById('elem');
-
-document.addEventListener('mousemove', function(event) {
-	elem.innerHTML = event.clientX + ' : ' + event.clientY;
-});
---
+++ script290.js
@@ -0,0 +1,13 @@
+let elem = document.querySelector('#elem');
+
+elem.addEventListener('click', func);
+elem.addEventListener('dblclick', func);
+
+function func() {
+	if (event.type == 'click'){
+		elem.style.color = 'green';
+	}
+	if (event.type == 'dblclick'){
+		elem.style.color = 'red';
+	}
+}
