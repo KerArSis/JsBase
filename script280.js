360551f3b00999a2140ed0c5c145d4c128a6645a
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-04-17 20:40:19 +0300

    task_281

--- script280.js
+++
@@ -1,8 +0,0 @@
-let elem = document.querySelector('#elem');
-
-elem.addEventListener('input', function() {
-	if (this.value.length > 5) {
-    	console.log(elem.value);
-	}
-		
-});
---
+++ script281.js
@@ -0,0 +1,16 @@
+let elem   = document.querySelector('#elem');
+let elem1   = document.querySelector('#elem1');
+
+
+elem.addEventListener('input', function func(){
+	if (elem.value.length === 2){
+		elem1.focus();
+	}
+})
+
+elem1.addEventListener('input', function func1(){
+	if (elem1.value.length === 2){
+		console.log('ggg')
+		elem1.blur();
+	}
+})
