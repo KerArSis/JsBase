6a2ceda1f9831665564a0d2eae27ffdd726c942e
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-04-17 20:49:30 +0300

    task_285

--- script284.js
+++
@@ -1,7 +0,0 @@
-let select = document.querySelector('#select');
-let button = document.querySelector('#button');
-let d = new Data(2023,3,1)
-
-button.addEventListener('click', function() {
-	select.value = alert(d);
-});
---
+++ script285.js
@@ -0,0 +1,5 @@
+let select = document.querySelector('#select');
+
+for (let option of select) {
+	console.log(option);
+}
