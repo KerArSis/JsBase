2ce38ea8d93230856318144d5795984fcf957264
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-04-17 21:36:49 +0300

    task_308

--- script307.js
+++
@@ -1,8 +0,0 @@
-let elem = document.querySelector('#elem');
-function func(param1, param2) {
-	console.log(this.value + param1 + param2);
-}
-let newFunc = func.bind(elem);
-newFunc('1', '2');
-
-func = func.bind(elem);
