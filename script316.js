0b769aea941357c6132e96b36635bcd8565aaf10
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-04-26 21:07:14 +0300

    task_317

--- script316.js
+++
@@ -1,8 +0,0 @@
-let elem = document.querySelector('#elem');
-let start = document.querySelector('#start');
-
-start.addEventListener('click', function() {
-	setInterval(function(self) {
-		console.log(self.value);
-	}, 1000, this);
-});
