446e62581a83919116cd1a9f09e1ac74dc1f7bbe
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-04-26 21:25:07 +0300

    task_324

--- script323.js
+++
@@ -1,9 +0,0 @@
-let parent = document.querySelector('#elem');
-let li = document.createElement('li');
-let li1 = document.createElement('li');
-
-li.innerHTML = 'end';
-li1.innerHTML = 'start';
-
-parent.append(li);
-parent.prepend(li1);
