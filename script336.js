edb05ff8836964ad16576be188cbdc98d68131a4
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-04-30 18:07:15 +0300

    task_337

--- script336.js
+++
@@ -1,7 +0,0 @@
-let elem   = document.querySelector('#elem');
-let remove = document.querySelector('#remove');
-
-remove.addEventListener('click', function(event) {
-	elem.remove();
-	event.preventDefault();
-});
